71abee0da088bfd888c6cb748c40bb33ff7a2771
Sevara Obidova <sevaraobidova505@gmail.com>
2023-12-11 16:18:21 +0500

    UI libraries are added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import '../../index.css'
 export default function Home() {
   return (
-    <div>
+    <div className='m-9 bg-indigo-500'>
       <h1>Home</h1>
     </div>
   )
