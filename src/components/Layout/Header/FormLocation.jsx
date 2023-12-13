9500bc775d6334782b5ea2d1fb0b60f6e7f99d2e
Mashhura <mashhura2020@gmail.com>
2023-12-13 22:46:51 +0500

    Revert "shadcn problem solved"
    This reverts commit b281cf5d55aa06d1399e5bc3d283e3071b600476.

--- src/components/Layout/Header/FormLocation.jsx
+++ src/components/Layout/Header/FormLocation.jsx
@@ -1,5 +1,3 @@
-import { Alert } from "@/src/components/ui/alert";
-import { Button } from "../../ui/button";
 
 
 
@@ -7,9 +5,7 @@ import { Button } from "../../ui/button";
 export default function FormLocation() {
     return ( 
         <>
-            <Alert>
-               <Button>send</Button> 
-          </Alert>
+            
         </>
         
      );
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { Button } from '../../ui/button'
 
 
 export default function Header() {
   return (
     <div className='container mx-auto py-4 '>
 
-      <div className='flex flex-row' >
-        <a className='basis-1/4 ' href=""><Link><img src="" alt="logo" /></Link></a>
-        <ul className=' flex flex-row basis-1/2 ' >
-          <li className='basis-1/4 font-link text-h4 underline' >
-            <NavLink to='/'>Главная</NavLink>
+    <div className='flex flex-row' >
+      <a className='basis-1/4 ' href=""><Link><img src="" alt="logo" /></Link></a>
+      <ul className=' flex flex-row basis-1/2 ' >
+        <li className='basis-1/4 font-link text-h4 underline' >
+          <NavLink to='/'>Главная</NavLink>
 
-          </li>
+        </li>
           <li className='basis-1/3 font-link text-h4 underline'>
-            <NavLink to='/shop'>Магазин товаров</NavLink>
+          <NavLink to='/shop'>Магазин товаров</NavLink>
 
-          </li>
-        </ul>
-        <Button>send</Button>
-        <form className='basis-1/4'>
-          <input type="text" placeholder='location' />
-        </form>
-        <form className='basis-1/4' action="">  <button>Войти в кабинет</button></form>
-      </div>
+        </li>
+      </ul>
+      <form className='basis-1/4'>
+        <input type="text" placeholder='location' />
+      </form>
+      <form className='basis-1/4' action="">  <button>Войти в кабинет</button></form>
+    </div>
     </div>
   )
 }
