bf3d0d4ba0d54717a46d689ce9677a5bc4a95453
Dilshoda <alimovadilshoda47@gmail.com>
2023-12-14 23:28:12 +0500

    button component is done

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,12 +1,10 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
-import { cn } from "../lib/utils";
-
-
+import { cn } from "../lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-bold ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 ",
   {
     variants: {
       variant: {
@@ -21,9 +19,9 @@ const buttonVariants = cva(
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-10 px-12 py-38 rounded-3xl w-fit",
+        sm: "h-9 rounded-full px-3",
+        lg: "h-11 rounded-full px-8",
         icon: "h-10 w-10",
       },
     },
