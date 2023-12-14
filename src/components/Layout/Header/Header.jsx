bca4da40616fc51a9b56324a4be3df378d80d58a
Mashhura <mashhura2020@gmail.com>
2023-12-15 01:03:22 +0500

    logo img added

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-
-
+import logo from '/src/assets/images/logo.svg'
 export default function Header() {
   return (
     <div className='container mx-auto py-4 '>
 
     <div className='flex flex-row' >
-      <a className='basis-1/4 ' href=""><Link><img src="" alt="logo" /></Link></a>
+      <a className='basis-1/4 ' href=""><Link><img src={logo} alt="" /></Link></a>
       <ul className=' flex flex-row basis-1/2 ' >
         <li className='basis-1/4 font-link text-h4 underline' >
           <NavLink to='/'>Главная</NavLink>
