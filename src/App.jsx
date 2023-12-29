c58a0333a79fd8c33f722e65ebf85b066a7d8757
Dilshoda <alimovadilshoda47@gmail.com>
2023-12-29 16:14:32 +0500

    configured profile routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
-import ShopProduct from './pages/Profile/components/ShopProduct/ShopProduct';
+import ShopProduct from './pages/Profile/components/ShoppingCart/ShoppingCart';
 import Profile from './pages/Profile/Profile';
 import Error from './pages/Error/Error'
+import OrderHistory from './pages/Profile/components/OrderHistory/OrderHistory';
+import OrderStatus from './pages/Profile/components/OrderStatus/OrderStatus';
+import PersonalInformation from './pages/Profile/components/PersonalInformation/PersonalInformation';
+import ShoppingCart from './pages/Profile/components/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
@@ -13,7 +17,12 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="shop" element={<ShopProduct />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="profile" element={<Profile />}>
+            <Route path='cart' element={<ShoppingCart/>}/>
+            <Route path='history' element={<OrderHistory/>}/>
+            <Route path='info' element={<PersonalInformation/>}/>
+            <Route path='status' element={<OrderStatus/>}/>
+          </Route>
           <Route path='*' element={<Error/>}/>
         </Route>
       </Routes>
