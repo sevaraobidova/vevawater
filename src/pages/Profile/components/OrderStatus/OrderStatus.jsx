1acc3b8219c06cbcecdda391bf700cdb36c19773
Dilshoda <alimovadilshoda47@gmail.com>
2024-01-04 17:48:26 +0500

    title component is done

--- src/pages/Profile/components/OrderStatus/OrderStatus.jsx
+++ src/pages/Profile/components/OrderStatus/OrderStatus.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Title from '../Title'
 
 export default function OrderStatus() {
   return (
     <div>
-      <h1>Оповещения</h1>
+      <Title text='Оповещения'/>
     </div>
   )
 }
