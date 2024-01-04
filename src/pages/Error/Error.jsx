332530db7a61e76a5c8ae75db244a9b8fb341f6a
Dilshoda <alimovadilshoda47@gmail.com>
2024-01-04 14:55:53 +0500

    errorPage mobile version

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Error() {
   return (
-    <div className='flex flex-col justify-center items-center gap-[30px] my-[80px] text-blue'>
-      <h1 className='text-[10rem]'>404</h1>
-      <p className='text-[2rem] animate-bounce'>Страница не найдена!</p>
+    <div className='flex flex-col justify-center items-center gap-[30px] my-[80px] text-blue container mx-auto text-center'>
+      <h1 className='text-[10rem] sm:text-[7rem]'>404</h1>
+      <p className='text-[2rem] animate-bounce sm:text-[1.5rem]'>Страница не найдена!</p>
     </div>
   )
 }
